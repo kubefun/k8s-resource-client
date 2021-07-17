03a61d8b9023577d39a4a5712c04c8290a106903
Wayne Witzel III <wayne@riotousliving.com>
2021-07-16 21:18:29 -0400

    add basic logging and typed error for restconfig
    Signed-off-by: Wayne Witzel III <wayne@riotousliving.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/wwitzel3/k8s-resource-client
 
 go 1.16
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/stretchr/testify v1.7.0
+	go.uber.org/zap v1.18.1
+	k8s.io/client-go v0.21.3
+)
--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -1,14 +1,60 @@
 package client_test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest"
+	"k8s.io/client-go/rest"
+
 	"github.com/stretchr/testify/assert"
+
 	"github.com/wwitzel3/k8s-resource-client/pkg/client"
+	"github.com/wwitzel3/k8s-resource-client/pkg/errors"
 )
 
+func TestNewClientNoRestConfig(t *testing.T) {
+	_, err := client.NewClient()
+	assert.ErrorIs(t, err, &errors.NilRESTConfig{})
+}
+
+func TestNewClientRestConfigWarnings(t *testing.T) {
+	burstWarning := false
+	qpsWarning := false
+
+	config := &rest.Config{QPS: 200, Burst: 400}
+	logger := zaptest.NewLogger(t, zaptest.WrapOptions(zap.Hooks(func(e zapcore.Entry) error {
+		fmt.Println(e.Message, e.Level)
+		if strings.Contains(e.Message, "QPS") && e.Level == zap.WarnLevel {
+			qpsWarning = true
+		} else if strings.Contains(e.Message, "Burst") && e.Level == zap.WarnLevel {
+			burstWarning = true
+		}
+		return nil
+	})))
+
+	c, err := client.NewClient(client.WithRestClientConfig(config), client.WithLogger(logger))
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	assert.NotNil(t, c)
+	assert.Equal(t, c.NamespaceMode, client.Auto)
+	assert.Equal(t, c.ResourceMode, client.Auto)
+	assert.Equal(t, c.SkipSubjectAccessChecks, false)
+	assert.True(t, qpsWarning)
+	assert.True(t, burstWarning)
+}
 func TestNewClient(t *testing.T) {
-	c := client.NewClient()
+	config := &rest.Config{QPS: 400, Burst: 800}
+	c, err := client.NewClient(client.WithRestClientConfig(config))
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
 	assert.NotNil(t, c)
 	assert.Equal(t, c.NamespaceMode, client.Auto)
 	assert.Equal(t, c.ResourceMode, client.Auto)
@@ -16,11 +62,17 @@ func TestNewClient(t *testing.T) {
 }
 
 func TestNewClientOptions(t *testing.T) {
-	c := client.NewClient(
+	config := &rest.Config{QPS: 400, Burst: 800}
+	c, err := client.NewClient(
 		client.WithNamespaceMode(client.Explicit),
 		client.WithResourceMode(client.Explicit),
 		client.WithSkipSubjectAccessChecks(true),
+		client.WithRestClientConfig(config),
 	)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
 	assert.NotNil(t, c)
 	assert.Equal(t, c.NamespaceMode, client.Explicit)
 	assert.Equal(t, c.ResourceMode, client.Explicit)
