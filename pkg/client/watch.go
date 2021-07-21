912e95ec1e976fe4a3eee438dde399a7db1a98e8
Wayne Witzel III <wayne@riotousliving.com>
2021-07-21 00:13:26 -0400

    add watcher files
    Signed-off-by: Wayne Witzel III <wayne@riotousliving.com>

---
+++ pkg/client/watch.go
@@ -0,0 +1,22 @@
+package client
+
+import (
+	"context"
+
+	"github.com/wwitzel3/k8s-resource-client/pkg/cache"
+	"github.com/wwitzel3/k8s-resource-client/pkg/resource"
+	"go.uber.org/zap"
+)
+
+func WatchResource(ctx context.Context, client *Client, res resource.Resource, queueEvents bool) *cache.WatchDetails {
+	client.Logger.Info("creating ListWatch",
+		zap.String("resource", res.APIResource.Name),
+	)
+	return client.watcher.Watch(ctx, res, queueEvents)
+}
+
+func WatchAllResources(ctx context.Context, client *Client, queueEvents bool) {
+	for _, res := range cache.Resources.Get("namespace") {
+		WatchResource(ctx, client, res, queueEvents)
+	}
+}
